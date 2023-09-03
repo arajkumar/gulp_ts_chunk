daf02641998b961f80ecb6465e8f0f9268d0d76a
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2023-09-03 13:10:05 +0000

    Refactor pgcopydb sql to COPY converter
    Signed-off-by: Arunprasad Rajkumar <ar.arunprasad@gmail.com>

--- pgcopydb_copy.py
+++ pgcopydb_copy.py
@@ -1,32 +1,8 @@
 import sys
-import csv
 import json
 
-from dataclasses import dataclass
-
-from collections import namedtuple
-
-Hypertable = namedtuple('Hypertable', ['schema', 'table'])
-
-@dataclass
-class State:
-    copy_inprogress: bool = False
-
-def read_ndjson(lines):
-    for line in lines:
-        try:
-            data = json.loads(line.strip())
-            yield data
-        except json.JSONDecodeError as e:
-            print(f"json decoder error {e}", file=sys.stderr)
-
-def _get_ht_id(chunk_name):
-    parts = chunk_name.split("_")
-    assert len(parts) > 2
-    return parts[2]
-
-
 def insert_statement_to_copy(insert):
+    # PREPARE b6175cf4 AS INSERT INTO "public"."tags" ("id", "name", "fleet", "driver", "model", "device_version", "load_capacity", "fuel_capacity", "nominal_fuel_consumption") overriding system value VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);
     tokens = insert.split('(')
     insert_prefix = tokens[0].split(' ')
     table_name = insert_prefix[5]
@@ -35,40 +11,43 @@ def insert_statement_to_copy(insert):
     return f"COPY {table_name}({columns}) FROM stdin;"
 
 def execute_to_copy_row(row):
+    # EXECUTE b6175cf4["592603","truck_0","East","Rodney","F-150","v1.5","2000","200","15"];
     row = row[16:][:-2]
     data = json.loads(row)
+    # replace None with \N, as per COPY format \N represents
+    # NULL
     data = map(lambda n: '\\N' if n is None else n, data)
+    # copy columns are separated by \t
     return '\t'.join(data)
 
 
-def transform_insert_to_copy(chunk_map, data, state):
-    # PREPARE b6175cf4 AS INSERT INTO "public"."tags" ("id", "name", "fleet", "driver", "model", "device_version", "load_capacity", "fuel_capacity", "nominal_fuel_consumption") overriding system value VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);
-    # EXECUTE b6175cf4["592603","truck_0","East","Rodney","F-150","v1.5","2000","200","15"];
-    prefix = data[:8].strip()
-    dml = data[20:26]
-    if prefix == 'PREPARE' and dml == 'INSERT':
-        if not state.copy_inprogress:
-            state.copy_inprogress = True
-            yield insert_statement_to_copy(data)
-    elif prefix == 'EXECUTE' and state.copy_inprogress:
-        yield execute_to_copy_row(data)
-    else:
-        if state.copy_inprogress:
-            yield '\.'
-        state.copy_inprogress = False
-        yield data
+def transform_insert_to_copy(lines):
+    copy_inprogress = False
+    for line in lines:
+        prefix = line[:8].strip()
+        dml = line[20:26]
+        if prefix == 'PREPARE' and dml == 'INSERT':
+            if not copy_inprogress:
+                copy_inprogress = True
+                yield insert_statement_to_copy(line)
+        elif prefix == 'EXECUTE' and copy_inprogress:
+            yield execute_to_copy_row(line)
+        else:
+            if copy_inprogress:
+                yield '\.'
+                copy_inprogress = False
+            yield line
 
-def write_ndjson(output, lines):
+def write(output, lines):
     for l in lines:
-        output.write(l + '\n')
+        output.write(l)
+        output.write('\n')
         output.flush()
 
 
-def gulp_timescale_chunks(chunk_map, lines):
-    state = State()
-    for line in lines:
-        yield from transform_insert_to_copy(chunk_map, line, state)
+def transform(lines):
+    yield from transform_insert_to_copy(lines)
 
 if __name__ == '__main__':
-    out_lines = gulp_timescale_chunks({}, sys.stdin)
-    write_ndjson(sys.stdout, out_lines)
+    out_lines = transform(sys.stdin)
+    write(sys.stdout, out_lines)
